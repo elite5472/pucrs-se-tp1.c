b50d2635db1d65e359c943a1127169eeb4a29bd8
Guillermo Borges <guiborges@live.com>
2016-04-19 23:34:16 -0300

    changes

--- build/hellfire/app/dummy_test.c
+++ build/hellfire/app/dummy_test.c
@@ -4,15 +4,17 @@ void task(void){
 	int32_t tid;
 
 	tid = HF_CurrentTaskId();
+	printf("T%d", tid);
+	int32_t x;
 	for (;;){
-		printf("T%d", tid);
+		i++;
 	}
 }
 
 void ApplicationMain(void){
 	HF_AddPeriodicTask(task, 4, 1, 4, "t1", 2048, 0, 0);
 	HF_AddPeriodicTask(task, 5, 2, 5, "t2", 2048, 0, 0);
-	HF_AddPeriodicTask(task, 7, 1, 5, "t3", 2048, 0, 0);
+	HF_AddPeriodicTask(task, 7, 1, 3, "t3", 2048, 0, 0);
 
 	HF_Start();
 }
